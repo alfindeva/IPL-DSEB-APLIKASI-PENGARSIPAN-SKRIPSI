01d864981065e68b1446b327464b69b65cfee1e5
Alfin G <119277850+alfindeva@users.noreply.github.com>
2022-12-04 09:31:46 +0700

    database rancangan aplikasi perpustakaan

--- db_perpustakaan.sql
+++ db_perpustakaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 01 Des 2022 pada 15.03
+-- Waktu pembuatan: 04 Des 2022 pada 03.29
 -- Versi server: 10.4.19-MariaDB
 -- Versi PHP: 8.0.6
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `t_data_anggota` (
-  `id` int(11) NOT NULL,
+  `id_anggota` int(11) NOT NULL,
   `nama` varchar(30) DEFAULT NULL,
   `email` varchar(30) DEFAULT NULL,
   `no_hp` varchar(13) DEFAULT NULL,
@@ -42,7 +42,7 @@ CREATE TABLE `t_data_anggota` (
 --
 
 CREATE TABLE `t_data_buku` (
-  `id` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
   `judul` varchar(30) DEFAULT NULL,
   `pengarang` varchar(30) DEFAULT NULL,
   `penerbit` varchar(30) DEFAULT NULL,
@@ -58,10 +58,11 @@ CREATE TABLE `t_data_buku` (
 --
 
 CREATE TABLE `t_peminjaman_buku` (
-  `id` int(11) NOT NULL,
+  `id_peminjaman` int(11) NOT NULL,
   `id_anggota` int(11) DEFAULT NULL,
-  `nama` varchar(30) DEFAULT NULL,
   `id_buku` int(11) DEFAULT NULL,
+  `id_pustakawan` int(11) DEFAULT NULL,
+  `nama` varchar(30) DEFAULT NULL,
   `judul` varchar(30) DEFAULT NULL,
   `jumlah` int(11) DEFAULT NULL,
   `tgl_pinjam` varchar(30) DEFAULT NULL,
@@ -77,7 +78,7 @@ CREATE TABLE `t_peminjaman_buku` (
 --
 
 CREATE TABLE `t_pustakawan` (
-  `id` int(11) NOT NULL,
+  `id_pustakawan` int(11) NOT NULL,
   `nama_pustakawan` varchar(30) DEFAULT NULL,
   `nama_pengguna` varchar(30) DEFAULT NULL,
   `kata_sandi` varchar(30) DEFAULT NULL
@@ -91,25 +92,28 @@ CREATE TABLE `t_pustakawan` (
 -- Indeks untuk tabel `t_data_anggota`
 --
 ALTER TABLE `t_data_anggota`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_anggota`);
 
 --
 -- Indeks untuk tabel `t_data_buku`
 --
 ALTER TABLE `t_data_buku`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_buku`);
 
 --
 -- Indeks untuk tabel `t_peminjaman_buku`
 --
 ALTER TABLE `t_peminjaman_buku`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_peminjaman`),
+  ADD KEY `id_anggota` (`id_anggota`),
+  ADD KEY `id_buku` (`id_buku`),
+  ADD KEY `id_pustakawan` (`id_pustakawan`);
 
 --
 -- Indeks untuk tabel `t_pustakawan`
 --
 ALTER TABLE `t_pustakawan`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_pustakawan`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -119,25 +123,37 @@ ALTER TABLE `t_pustakawan`
 -- AUTO_INCREMENT untuk tabel `t_data_anggota`
 --
 ALTER TABLE `t_data_anggota`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `t_data_buku`
 --
 ALTER TABLE `t_data_buku`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `t_peminjaman_buku`
 --
 ALTER TABLE `t_peminjaman_buku`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `t_pustakawan`
 --
 ALTER TABLE `t_pustakawan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pustakawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `t_peminjaman_buku`
+--
+ALTER TABLE `t_peminjaman_buku`
+  ADD CONSTRAINT `t_peminjaman_buku_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `t_data_anggota` (`id_anggota`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `t_peminjaman_buku_ibfk_2` FOREIGN KEY (`id_buku`) REFERENCES `t_data_buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `t_peminjaman_buku_ibfk_3` FOREIGN KEY (`id_pustakawan`) REFERENCES `t_pustakawan` (`id_pustakawan`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
